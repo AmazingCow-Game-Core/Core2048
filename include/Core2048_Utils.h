75899d2a94c2e4673fe8ab624394909ce407d3b2
n2omatt <n2o.matt@gmail.com>
2017-05-26 01:02:04 -0300

    - Make the COW_CORE2048_VERSION get the values from the other macros.

--- include/Core2048_Utils.h
+++ include/Core2048_Utils.h
@@ -43,14 +43,21 @@
 //We use MACROS so is easier to change if needed.
 //Is (in our opinion) more explicit.
 //And finally the editors will not reformat the code.
-
 #define NS_CORE2048_BEGIN namespace Core2048 {
 #define NS_CORE2048_END   }
 #define USING_NS_CORE2048 using namespace Core2048
 
+
 //The core version number.
+//  Do not edit manually this...
+//  Instead it should be changed by update_version.sh script
+//  because it'll update the Doxygen stuff as well.
 #define COW_CORE2048_VERSION_MAJOR    "0"
 #define COW_CORE2048_VERSION_MINOR    "1"
 #define COW_CORE2048_VERSION_REVISION "0"
 
-#define COW_CORE2048_VERSION "0.1.0"
+#define COW_CORE2048_VERSION       \
+    COW_CORE2048_VERSION_MAJOR "." \
+    COW_CORE2048_VERSION_MINOR "." \
+    COW_CORE2048_VERSION_REVISION
+
